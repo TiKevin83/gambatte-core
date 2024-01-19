ae56367663b31f79c375ad78cca67b036b2ce497
TiKevin83 <travismcgeehan@gmail.com>
2024-01-19 00:20:07 -0500

    Learn how to interface with char pointers and avoid exposing FS

--- scripts/build_shlib.sh
+++ scripts/build_shlib.sh
@@ -3,4 +3,6 @@ set -e
 echo "cd libgambatte && scons shlib $1"
 cd "$(dirname "$0")/../libgambatte"
 scons shlib $1
+cp libgambatte.js ../public/libgambatte.js
+cp libgambatte.wasm ../public/libgambatte.wasm
 ../scripts/.copy_bizhawk_after_build.sh
